f43ac761e3c09d52d3ea046eb984246b6aad4520
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-10-05 08:33:29 +0530

    admin-permission card added

--- frontend/src/app/admin/test/test.component.ts
+++ frontend/src/app/admin/test/test.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-
+  participate = [
+    {
+      participateImg: '../../../assets/images/png-transparent-graphic-design-icon-customer-service-avatar-icon-design-call-centre-yellow-smile-forehead.png',
+      logo: '../../../assets/images/participate-logo2.png',
+      name: 'CEO of Google',
+      contant: 'AI tools',
+      btn: 'Participate',
+    },
+    {
+      participateImg: '../../../assets/images/png-transparent-graphic-design-icon-customer-service-avatar-icon-design-call-centre-yellow-smile-forehead.png',
+      logo: '../../../assets/images/participate-logo2.png',
+      name: 'CEO of Google',
+      contant: 'AI tools',
+      btn: 'Participate',
+    },
+    {
+      participateImg: '../../../assets/images/png-transparent-graphic-design-icon-customer-service-avatar-icon-design-call-centre-yellow-smile-forehead.png',
+      logo: '../../../assets/images/participate-logo2.png',
+      name: 'CEO of Google',
+      contant: 'AI tools',
+      btn: 'Participate',
+    },
+    {
+      participateImg: '../../../assets/images/png-transparent-graphic-design-icon-customer-service-avatar-icon-design-call-centre-yellow-smile-forehead.png',
+      logo: '../../../assets/images/participate-logo2.png',
+      name: 'CEO of Google',
+      contant: 'AI tools',
+      btn: 'Participate',
+    },
+   
+  ];
 }
